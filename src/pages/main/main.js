b745ae36793bbf3207268cc443c53500c12b8793
Jefferson <jbds.web@gmail.com>
2019-09-21 20:00:18 -0300

    Adicionando estrutura de push da API para objeto.

--- src/pages/main/main.js
+++ src/pages/main/main.js
@@ -26,6 +26,7 @@ export default class Main extends Component {
     const { users } = this.state;
     const result = users.map(user => (user.login));
     
+    var collection = [];
 
     for (const a of result) {
       const profiles = await api.get(`/users/${a}` , {
@@ -34,26 +35,26 @@ export default class Main extends Component {
            password: '258webVOXUS' 
         }
      });
+     collection.push(profiles.data);
 
-      this.setState({ links: profiles.data });
-
-      const { links } = this.state
-      console.log(links);
     };
 
+    this.setState({ links: collection });
 
 
   };
 
   render() {
-    const { users } = this.state;
+    const { links } = this.state;
     return (
       <div className="user-list">
         <input></input>
         <button></button>
-          {users.map(user => (
-              <div key={user.id} className="user-card">
-            <h1> {user.login}</h1>
+          {links.map(link => (
+              <div key={link.id} className="user-card">
+            <h1> {link.login}</h1>
+            <h1> {link.name}</h1>
+            <img alt="teste" src={link.avatar_url}/>
             </div>
           ))}
         
